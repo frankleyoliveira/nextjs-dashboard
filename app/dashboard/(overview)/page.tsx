723ae24188c2fa42dcab1aa60ce9da35a554cd89
Frankley Oliveira <frankleyj.o@gmail.com>
2025-06-18 18:02:42 -0300

    format code with prettier

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,13 @@
-import RevenueChart from "@/app/ui/dashboard/revenue-chart"
-import LatestInvoices from "@/app/ui/dashboard/latest-invoices"
-import { lusitana } from "@/app/ui/fonts"
-import { Suspense } from "react"
+import RevenueChart from '@/app/ui/dashboard/revenue-chart'
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices'
+import { lusitana } from '@/app/ui/fonts'
+import { Suspense } from 'react'
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
   CardsSkeleton,
-} from "@/app/ui/skeletons"
-import CardWrapper from "@/app/ui/dashboard/cards"
+} from '@/app/ui/skeletons'
+import CardWrapper from '@/app/ui/dashboard/cards'
 
 export default async function Page() {
   return (
